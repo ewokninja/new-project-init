c1e938e73d80b8ed091d28e93e92fe50c44fd031
Jonathan Tom <jt@jonathan-tom.com>
2020-02-28 22:46:39 -0500

    First working commit

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+# ----- Configure things here -----
+
+PROJ_DIR="/home/jt/bitbucket"
+PY_CREATE="/home/jt/bitbucket/new-project-init"
+BB_PATH="git@github.com:ewokninja"
+
+# ----- Nothing configurable after this line -----
+
+
 function create() {
+    # Start from somewhere we know... home.
     cd
-    python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
+
+    # Run the python script to handle the creation of the Github repo
+    python $PY_CREATE/create.py $1
+
+    # Move to the newly created project directory
+    cd $PROJ_DIR/$1
+
+    # Initialize the repo
     git init
-    git remote add origin git@github.com:KalleHallden/$1.git
+
+    # Set the origin
+    git remote add origin $BB_PATH/$1.git
+
+    # Setup an empty readme file
     touch README.md
+
+    # Add the readme file and commit/push it to the repo
     git add .
     git commit -m "Initial commit"
     git push -u origin master
+
+    # Start VS code in the new project's directory
     code .
 }
--- create.py
+++ create.py
@@ -1,17 +1,26 @@
 import sys
 import os
 from github import Github
+from dotenv import load_dotenv
 
-path = "/Users/kalle/Documents/Projects/MyProjects/"
+# Load the environment variables from the .env file
+load_dotenv()
 
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+# Assign the environment variables to variables
+project_path = os.getenv("PROJECT_PATH")
+username = os.getenv("GITHUB_USER")
+password = os.getenv("GITHUB_PASS")
 
 def create():
+    # Create the project's folder
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName)
+    os.makedirs(project_path + str(folderName)) 
+
+    # Create the github repo
     user = Github(username, password).get_user()
     repo = user.create_repo(folderName)
+
+    # Let the user know the repo was created on github
     print("Succesfully created repository {}".format(folderName))
 
 if __name__ == "__main__":
